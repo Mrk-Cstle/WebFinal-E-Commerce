51e3cecf9d2ea0548aede6b2581f04ab6d7c7577
Mrk-Cstle <castillo.markdavid64@gmail.com>
2023-05-22 00:45:16 +0800

    Update addProduct.php

--- login/addProduct.php
+++ login/addProduct.php
@@ -20,159 +20,161 @@
 </head>
 
 <style>
-.backR {
-    background-color: lightyellow;
-    background-size: cover;
-    background-position: center;
-    background-attachment: fixed;
-    background-repeat: no-repeat;
-}
-.prodName {
-    padding: 12px 0px 10px 10px;
-    font-family: Arial, Helvetica, sans-serif;
-    width: 40%;
-    margin-left: 50px;
-}
+    .backR {
+        background-color: lightyellow;
+        background-size: cover;
+        background-position: center;
+        background-attachment: fixed;
+        background-repeat: no-repeat;
+    }
 
-.prodInput {
-    padding: 6px 0 10px 10px;
-    font-family: Arial, Helvetica, sans-serif;
-    border-radius: 12px;
-    width: 30%;
-    margin-left: 50px;
-}
+    .prodName {
+        padding: 12px 0px 10px 10px;
+        font-family: Arial, Helvetica, sans-serif;
+        width: 40%;
+        margin-left: 50px;
+    }
 
-.prodPrice {
-    padding: 6px 0 10px 10px;
-    font-family: Arial, Helvetica, sans-serif;
-    border-radius: 12px;
-    width: 17%;
-    margin-left: 50px;
-}
+    .prodInput {
+        padding: 6px 0 10px 10px;
+        font-family: Arial, Helvetica, sans-serif;
+        border-radius: 12px;
+        width: 30%;
+        margin-left: 50px;
+    }
 
-.prodUpload {
-    margin-bottom: 20px;
-    margin-left: 50px;
-    font-family: Arial, Helvetica, sans-serif;
-}
+    .prodPrice {
+        padding: 6px 0 10px 10px;
+        font-family: Arial, Helvetica, sans-serif;
+        border-radius: 12px;
+        width: 17%;
+        margin-left: 50px;
+    }
 
-p {
-    margin-left: 50px;
-    margin-bottom: 50px;
-    font-size: 20px;
-    font-weight: bolder;
-    font-family: Arial, Helvetica, sans-serif;
-    
-}
+    .prodUpload {
+        margin-bottom: 20px;
+        margin-left: 50px;
+        font-family: Arial, Helvetica, sans-serif;
+    }
 
-.imgPath {
-    display: static;
-    width: auto;
-}
+    p {
+        margin-left: 50px;
+        margin-bottom: 50px;
+        font-size: 20px;
+        font-weight: bolder;
+        font-family: Arial, Helvetica, sans-serif;
 
-tr, th{
-    column-gap: 40px;
-    border: 1px solid black;
-    text-align: center;
-    justify-content: center;
-}
+    }
 
-td {
-    padding-top: 20px;
-    font-family: 'poppins' ,sans-serif;
-    border: 1px solid black;
-    text-align: center;
-    justify-content: center;
-}
+    .imgPath {
+        display: static;
+        width: auto;
+    }
 
-#align {
-    padding-top: 90px;
-    text-align: center;
-    justify-content: center;
-}
+    tr,
+    th {
+        column-gap: 40px;
+        border: 1px solid black;
+        text-align: center;
+        justify-content: center;
+    }
 
-h3 {
-    margin-left: 60px;
-    display: flex;
-}
+    td {
+        padding-top: 20px;
+        font-family: 'poppins', sans-serif;
+        border: 1px solid black;
+        text-align: center;
+        justify-content: center;
+    }
 
-img {
-    width: auto;
-    height: 200px;
-}
+    #align {
+        padding-top: 90px;
+        text-align: center;
+        justify-content: center;
+    }
 
+    h3 {
+        margin-left: 60px;
+        display: flex;
+    }
+
+    img {
+        width: auto;
+        height: 200px;
+    }
 </style>
 
 <body>
     <?php
     include 'include/nav.php'; ?>
-    <?php if (isset($_GET['error'])) : ?>
-    <?php endif ?>
+
     <div class="backR">
-    <form action="addProdDb.php" method="POST" enctype="multipart/form-data">
+        <form action="addProdDb.php" method="POST" enctype="multipart/form-data">
 
-        <label class="prodName d-flex" for="prodName">Product Name: </label>
-        <input class="prodInput d-flex" type="text" name="prodName" id="prodName" />
+            <label class="prodName d-flex" for="prodName">Product Name: </label>
+            <input class="prodInput d-flex" type="text" name="prodName" id="prodName" />
 
-        <label class="prodName d-flex" for="brand">Brand: </label>
-        <input class="prodInput d-flex" type="text" name="brand" id="brand" />
+            <label class="prodName d-flex" for="brand">Brand: </label>
+            <input class="prodInput d-flex" type="text" name="brand" id="brand" />
 
-        <label class="prodName d-flex" for="prodPrice">Price: </label>
-        <input class="prodPrice d-flex" type="number" name="prodPrice" id="prodPrice" />
+            <label class="prodName d-flex" for="prodPrice">Price: </label>
+            <input class="prodPrice d-flex" type="number" name="prodPrice" id="prodPrice" />
 
-        <label class="prodName d-flex" for="my_image">Product Image: </label>
-        <input class="prodInput d-flex" type="file" name="my_image">
+            <label class="prodName d-flex" for="my_image">Product Image: </label>
+            <input class="prodInput d-flex" type="file" name="my_image">
 
-        <label class="prodName d-flex" for="prodDescrpition">Product Description: </label>
-        <input class="prodInput d-flex" type="text" name="prodDescrpition" id="prodDescrpition" />
+            <label class="prodName d-flex" for="prodDescrpition">Product Description: </label>
+            <input class="prodInput d-flex" type="text" name="prodDescrpition" id="prodDescrpition" />
 
-        <input class="prodUpload d-flex mt-4" type="submit" name="submit" value="Upload">
-        <p><?php echo $_GET['error']; ?></p>
+            <input class="prodUpload d-flex mt-4" type="submit" name="submit" value="Upload">
+            <?php if (isset($_GET['error'])) : ?>
+                <p><?php echo $_GET['error']; ?></p>
+            <?php endif ?>
 
-    </form>
+        </form>
 
-    <div>
-        <?php
-        include 'include/selectDb.php';
-        ?>
-        <h3>Product List</h3>
-        <table class="table table-hover">
-            <tr>
-                <th>Image</th>
-                <th>Product Name</th>
-                <th>Brand</th>
-                <th>Price</th>
-                <th>Description</th>
-            </tr>
+        <div>
             <?php
-            if (mysqli_num_rows($resultProd) > 0) {
-
-                while ($row  = $resultProd->fetch_assoc()) {
-
+            include 'include/selectDb.php';
             ?>
+            <h3>Product List</h3>
+            <table class="table table-hover">
+                <tr>
+                    <th>Image</th>
+                    <th>Product Name</th>
+                    <th>Brand</th>
+                    <th>Price</th>
+                    <th>Description</th>
+                </tr>
+                <?php
+                if (mysqli_num_rows($resultProd) > 0) {
 
-                    <tr>
-                        <td id="imgPath"><img src="uploads/<?= $row['file_path'] ?>"></td>
-                        <td id="align"><?php echo $row['fname']; ?></td>
-                        <td id="align"><?php echo $row['brand']; ?></td>
-                        <td id="align"><?php echo $row['price']; ?></td>
-                        <td id="align"><?php echo $row['info']; ?></td>
-                        <td id="align"><a href="createStaffDelete.php?id=<?php echo $row['product_id']; ?>">Remove</a></td>
+                    while ($row  = $resultProd->fetch_assoc()) {
 
-                    </tr>
+                ?>
+
+                        <tr>
+                            <td id="imgPath"><img src="uploads/<?= $row['file_path'] ?>"></td>
+                            <td id="align"><?php echo $row['fname']; ?></td>
+                            <td id="align"><?php echo $row['brand']; ?></td>
+                            <td id="align"><?php echo $row['price']; ?></td>
+                            <td id="align"><?php echo $row['info']; ?></td>
+                            <td id="align"><a href="createStaffDelete.php?id=<?php echo $row['product_id']; ?>">Remove</a></td>
+
+                        </tr>
 
 
 
 
-            <?php
+                <?php
+                    }
+                    echo "</table>";
+                } else {
+                    echo "<tr><td>" . "0 result" . "</td><td></table>";
                 }
-                echo "</table>";
-            } else {
-                echo "<tr><td>" . "0 result" . "</td><td></table>";
-            }
-            ?>
-        </table>
-    </div>
+                ?>
+            </table>
+        </div>
     </div>
 
 
