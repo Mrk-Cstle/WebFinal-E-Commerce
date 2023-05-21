e7b46912d29b6de2ea7181adcdc417c8718e2d6d
Mrk-Cstle <castillo.markdavid64@gmail.com>
2023-05-21 20:49:41 +0800

    Update ecommerce.sql

--- ecommerce.sql
+++ ecommerce.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 20, 2023 at 11:19 AM
--- Server version: 10.4.27-MariaDB
--- PHP Version: 8.1.12
+-- Generation Time: May 21, 2023 at 02:49 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -39,29 +39,6 @@ CREATE TABLE `login` (
 INSERT INTO `login` (`user`, `password`) VALUES
 ('qwe', 'qwe');
 
--- --------------------------------------------------------
-
---
--- Table structure for table `product`
---
-
-CREATE TABLE `product` (
-  `product_id` int(11) NOT NULL,
-  `fname` varchar(50) NOT NULL,
-  `file_path` varchar(50) NOT NULL,
-  `price` int(11) NOT NULL,
-  `date` datetime NOT NULL DEFAULT current_timestamp(),
-  `info` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `product`
---
-
-INSERT INTO `product` (`product_id`, `fname`, `file_path`, `price`, `date`, `info`) VALUES
-(33, '', 'C:fakepathHandwritten_2022-08-15_160851.jpg', 0, '2023-05-20 17:14:22', ''),
-(34, '', 'C:fakepathScreenshot_20221215_153607.jpg', 0, '2023-05-20 17:16:35', '');
-
 --
 -- Indexes for dumped tables
 --
@@ -71,22 +48,6 @@ INSERT INTO `product` (`product_id`, `fname`, `file_path`, `price`, `date`, `inf
 --
 ALTER TABLE `login`
   ADD PRIMARY KEY (`user`);
-
---
--- Indexes for table `product`
---
-ALTER TABLE `product`
-  ADD PRIMARY KEY (`product_id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `product`
---
-ALTER TABLE `product`
-  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
