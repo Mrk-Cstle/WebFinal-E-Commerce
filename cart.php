50895c4d7b5a3e22c97239bf5923a54111a334f6
Mrk-Cstle <castillo.markdavid64@gmail.com>
2023-05-22 00:39:03 +0800

    cart & shop

--- cart.php
+++ cart.php
@@ -1,8 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<?php include "assets/include/session.php"?>
+<?php include "assets/include/session.php" ?>
 <?php include 'assets/include/nav.php'; ?>
+
 <head>
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
@@ -21,46 +22,47 @@
 </head>
 
 <body>
-<div class="container">
-  <table class="table my-3">
-    <a href="emptycart.php" class="btn btn-sm btn-danger mt-2">Empty Cart</a>
-    <thead>
-      <tr class="text-center">
-        <th>S.no</th>
-        <th>Product Name</th>
-        <th>Quantity</th>
-        <th colspan="2">Action</th>
-      </tr>
-    </thead>
+  <div class="container">
+    <table class="table my-3">
+      <a href="emptycart.php" class="btn btn-sm btn-danger mt-2">Empty Cart</a>
+      <thead>
+        <tr class="text-center">
+          <th>S.no</th>
+          <th>Product Name</th>
+          <th>Quantity</th>
+          <th colspan="2">Action</th>
+        </tr>
+      </thead>
 
-    <tbody>
-      <?php
-      if (isset($_SESSION['cart'])) :
-        $i = 1;
-        foreach ($_SESSION['cart'] as $cart) :
-      ?>
-          <tr class="text-center">
-            <td><?php echo $i; ?> # </td>
-            <td> Product <?= $cart['pro_id']; ?></td>
-            <td>
-              <form action="update.php" method="post">
-                <input type="number" value="<?= $cart['qty']; ?>" name="qty" min="1">
-                <input type="hidden" name="upid" value="<?= $cart['pro_id']; ?>">
-            </td>
-            <td>
-              <input type="submit" name="update" value="Update" class="btn btn-sm btn-warning">
-              </form>
-            </td>
-            <td><a class="btn btn-sm btn-danger" href="removecartitem.php?id=<?= $cart['pro_id']; ?>">Remove</a></td>
-          </tr>
-      <?php
-          $i++;
-        endforeach;
-      endif;
-      ?>
-    </tbody>
-  </table>
-</div>
+      <tbody>
+        <?php
+        if (isset($_SESSION['cart'])) :
+          $i = 1;
+          foreach ($_SESSION['cart'] as $cart) :
+        ?>
+            <tr class="text-center">
+              <td><?php echo $i; ?> # </td>
+              <td> Product <?= $cart['pro_id']; ?></td>
+              <td>
+                <form action="update.php" method="post">
+                  <input type="number" value="<?= $cart['qty']; ?>" name="qty" min="1">
+                  <input type="hidden" name="upid" value="<?= $cart['pro_id']; ?>">
+              </td>
+              <td>
+                <input type="submit" name="update" value="Update" class="btn btn-sm btn-warning">
+                </form>
+              </td>
+              <td><a class="btn btn-sm btn-danger" href="removecartitem.php?id=<?= $cart['pro_id']; ?>">Remove</a></td>
+            </tr>
+        <?php
+            $i++;
+          endforeach;
+        endif;
+        ?>
+      </tbody>
+    </table>
+  </div>
 </body>
 <?php include 'footerS.php'; ?>
+
 </html>
